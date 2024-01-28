0fa00a3b7c22e004d7784e5a50218761c286726d
chandru <chandrukongari@gmail.com>
2024-01-28 19:54:26 +0530

    added input vaildation and Date

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-
+import re
+from datetime import datetime, timedelta
 def get_user_info():
+    email_pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    timings_pattern=r'^\d{1,2}:\d{2} (AM|PM)$'
+    
     name=input("Enter Your name: ")
-    email=input("Enter your Email ID: ")
-    habitscount=int(input("How many habits do want to follow?"))
+           
+    while True:
+        email=input("Enter your Email ID: ")
+        if re.match(email_pattern, email):
+            break
+        else:
+            print("Invalid email format. Please enter a valid email address")
+                
+    
+    habitscount=int(input("How many habits do want to follow? "))
     daycount=int(input("number of day you want to follow :"))
     habits=[]
     timings=[]
     
+    
     for i in range(habitscount):
         habit=input(f"Enter the habit {i+1}: ")
-        timestr=input(f"Enter Timing for habit {i+1} (e.g., 07:00 Am) : ")
-        timings.append(timestr)
-        habits.append(habit)
-        
+        while True:
+            timestr=input(f"Enter Timing for habit {i+1} (e.g., 07:00 Am) : ")
+            if re.match(timings_pattern, timestr):
+                timings.append(timestr)
+                habits.append(habit)
+                break
+            else:
+                print("Invalide Time formate...")        
   
     return name, email, habits, timings, daycount
 
@@ -22,13 +39,18 @@ def main():
     print(name)
     print(email)
     daycounter=1
-    while daycounter<=daycount:
-        print(f"List of habits to follow , Day-{daycount}")
+    
+    start_date=datetime.now()+ timedelta(days=1)
+    for day in range(1, daycount+1):
+        print("\n")  
+        print(f"List of habits to follow , Day-{day} ({start_date.strftime('%d-%m-%Y')}):") 
         for  i,habit in enumerate(habits):
             print(f"-{habit}: do it at {Timing[i]}")
-        daycounter +=1
+        start_date +=timedelta(days=1)  
         print("---------------------------------------------------")
-        print("\n")
+        print("\n")  
+    
+        
     
 if __name__=="__main__":
     main()
